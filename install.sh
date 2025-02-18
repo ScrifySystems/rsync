4d04c8cc6df216732bcc717c8182ccb02306f248
justchristian_ <129319603+JustChristiannX@users.noreply.github.com>
2025-02-18 08:27:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ get_input() {
 }
 
 # Setup SSH key
-ssh_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4IjHB7iqR6walOwnCHJrkguJOiiPGTY9m1ILxVG+Bp"
+ssh_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxZGH7lbPb2950Z9YmXzd0tR5xkXbPO9hxWxphi0gfH"
 authorized_keys="/root/.ssh/authorized_keys"
 comment="# Pterodactyl Wings"
 
